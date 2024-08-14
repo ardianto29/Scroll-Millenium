54a7f0b8f11358a7461720e367267e341afd8baf
ardiantoramadhan <56662573+ardianto29@users.noreply.github.com>
2024-08-14 10:34:44 +0700

    feat(section): add Banner

---
+++ src/sections/Banner.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import styled from "styled-components";
+
+const Section = styled.section`
+  min-height: 100vh;
+  width: 80vw;
+  margin: 0 auto;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`;
+
+const Container = styled.div`
+  min-height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+
+const BannerComponent = styled.h1`
+  font-size: 60px;
+  font-family: "Kaushan Script";
+  color: ${(props) => props.theme.text};
+  white-space: nowrap;
+  text-transform: uppercase;
+  line-height: 1;
+
+  span {
+    display: block;
+    background-color: ${(props) => props.theme.body};
+    padding: 1rem 2rem;
+  }
+`;
+
+const Banner = () => {
+  return (
+    <Section>
+      <Container id="up">
+        <BannerComponent>
+          <span
+            data-scroll
+            data-scroll-direction="horizontal"
+            data-scroll-speed="8"
+            data-scroll-target="#up">
+            I am not speaking of likes or dislikes... only facts.
+          </span>
+        </BannerComponent>
+        <BannerComponent>
+          <span
+            data-scroll
+            data-scroll-direction="horizontal"
+            data-scroll-speed="-6"
+            data-scroll-target="#up">
+            Something tells me my girls are at it again...
+          </span>
+        </BannerComponent>
+        <BannerComponent>
+          <span
+            data-scroll
+            data-scroll-direction="horizontal"
+            data-scroll-speed="6"
+            data-scroll-target="#up">
+            Oh, do you need my help?
+          </span>
+        </BannerComponent>
+        <BannerComponent>
+          <span
+            data-scroll
+            data-scroll-direction="horizontal"
+            data-scroll-speed="-4"
+            data-scroll-target="#up">
+            I'm going to destroy you!
+          </span>
+        </BannerComponent>
+        <BannerComponent>
+          <span
+            data-scroll
+            data-scroll-direction="horizontal"
+            data-scroll-speed="6"
+            data-scroll-target="#up">
+            I can see the Scenario to Victory!
+          </span>
+        </BannerComponent>
+      </Container>
+    </Section>
+  );
+};
+
+export default Banner;
